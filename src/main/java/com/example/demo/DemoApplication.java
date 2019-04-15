c3f03956569e3ff5c0c6d4282daef0ee5dca8b69
keyvan pishevar <kpisheva@redhat.com>
2019-04-15 16:01:52 -0500

    add  couple path to replay to rest

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,10 +2,24 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @SpringBootApplication
+@RestController
+
 public class DemoApplication {
 
+	@GetMapping("/")
+    public String welcome() {
+        return "Greetings from Spring Boot!";
+    }
+    
+    @GetMapping("/{input}")
+    public String welcome(@PathVariable String input) {
+        return "Greetings from Spring Boot to " + input;
+    }
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
